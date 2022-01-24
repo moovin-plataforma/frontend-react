4af909e2ad071e7abfe7e3913220f0966f9808ed
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-24 00:13:32 -0300

    Lendo API e fazendo paginacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.scss'
-// import { Table } from './components/Table'
+import Table  from './components/Table'
 
 const App: React.FC = () => {
 	const logoMoovin =
 		'https://cdn.moovin.com.br/project/manager-panel/img/logo-moovin.svg'
-
 	return (
 		<div>
 			<header>Logo Moovin</header>
+			<p>Últimas postagens</p>
+			<Table />
 		</div>
 	)
 }
--- src/context/ApiContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const ApiContext = createContext();
-
-export default ApiContext;
